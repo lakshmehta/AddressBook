cb6c4b7991016e683c2067e2e0e20d06bbf637ae
unknown <lakshmehta06@gmail.com>
2021-04-07 14:49:59 +0530

    [Laksh added UC5]

--- AddressBookMB/AddressBookMB/Program.cs
+++ AddressBookMB/AddressBookMB/Program.cs
@@ -8,19 +8,48 @@ namespace AddressBookMB
         public static Dictionary<string, List<Contacts>> addressBookStore = new Dictionary<string, List<Contacts>>();
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Address Book");
-            String bookName = Console.ReadLine();
-            AddressBookMain.addBook(bookName);
-            Console.WriteLine("Enter the name fo the address book you want to edit.");
-            String bookEdit = Console.ReadLine();
-            Console.WriteLine("Enter the first name of the person whose record you want to edit.");
-            String recordNameEdit = Console.ReadLine();
-            AddressBookMain.editRecord(recordNameEdit, bookEdit);
-            Console.WriteLine("Enter address book you want to delete record from");
-            String bookToDeleteIn = Console.ReadLine();
-            Console.WriteLine("Enter first name of the person whose record you want to delete.");
-            String recordNameToDelete = Console.ReadLine();
-            AddressBookMain.deleteRecord(recordNameToDelete, bookToDeleteIn);
+            Console.WriteLine("Wellcome to address book system program !");
+            int choice = 0;
+            while (choice != 4)
+            {
+                Console.WriteLine("1.Add Contacts");
+                Console.WriteLine("2.Edit Existing Contact");
+                Console.WriteLine("3.Delete Person Details");
+                Console.WriteLine("4.Close AddB");
+                choice = Convert.ToInt32(Console.ReadLine());
+
+                switch (choice)
+                {
+                    case 1:
+                        Console.WriteLine("Enter name of the address book in which you want to add record.");
+                        String bookName = Console.ReadLine();
+                        AddressBookMain.addBook(bookName);
+                        break;
+                    case 2:
+                        Console.WriteLine("Enter the name fo the address book you want to edit.");
+                        String bookToEdit = Console.ReadLine();
+                        Console.WriteLine("Enter the first name of the person whose record you want to edit.");
+                        String recordNameToEdit = Console.ReadLine();
+                        AddressBookMain.editRecord(bookToEdit, recordNameToEdit);
+                        break;
+                    case 3:
+                        Console.WriteLine("Enter name of the address book you want to delete record in.");
+                        String bookToDeleteIn = Console.ReadLine();
+                        Console.WriteLine("Enter first name of the person whose record you want to delete.");
+                        String recordNameToDelete = Console.ReadLine();
+                        AddressBookMain.deleteRecord(recordNameToDelete, bookToDeleteIn);
+                        break;
+                    case 4:
+                        choice = 4;
+                        break;
+                    default:
+                        Console.WriteLine("Invalid choice !");
+                        break;
+                }
+            }
+
+
+
 
         }
     }
